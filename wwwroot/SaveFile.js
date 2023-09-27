51a683c230746dbd77fa33d0a75676a5f2242334
Rakesh Kumar <ariel.rakeshkumar@gmail.com>
2023-09-27 18:45:36 +0530

    Add project files.

---
+++ wwwroot/SaveFile.js
@@ -0,0 +1,8 @@
+﻿function SaveFile(fileName, fileContent, fileExtension) {
+    var link = document.createElement('a');
+    link.download = fileName;
+    link.href = "data:application/" + fileExtension + ";charset=utf-8," + encodeURIComponent(fileContent);
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+};
